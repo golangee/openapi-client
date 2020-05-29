a813299d9d5041d0c2907fd07515bb0203d64322
Torben Schinke <torben.schinke@worldiety.de>
2020-05-29 18:16:11 +0200

    emitting structured error type to inspect server errors properly

--- async/apiroot.go
+++ async/apiroot.go
@@ -25,7 +25,12 @@ func emitApiRoot(f *gen.GoGenFile, doc *v3.Document) (string, error) {
 	f.ImportName("io", "")
 	f.ImportName("net/http", "")
 	f.ImportName("net/url", "")
+	f.ImportName("io/ioutil", "")
+	f.ImportName("reflect", "")
+	//f.ImportName("log","")
 
+	f.Printf(errTypeStub)
+	f.Printf("\n")
 	f.Printf(gen.Comment(doc.Info.Description))
 	rootName := gen.Public(doc.Info.Title + "Service")
 	f.Printf(parentClientStub, rootName)
@@ -71,8 +76,19 @@ func (s *%[1]s) doJson(req *http.Request, v interface{}) (*http.Response, error)
 		return nil, err
 	}
 	defer resp.Body.Close()
-	err = json.NewDecoder(resp.Body).Decode(v)
-	return resp, err
+
+	if resp.StatusCode == 200 {
+		err = json.NewDecoder(resp.Body).Decode(v)
+		return resp, err
+	}else{
+		err := ParseError(resp.Body)
+		return resp, err
+	}
+	
 }
 
+
 `
+
+// copied from http/error.go
+const errTypeStub = "// Error describes a (nested) server error\ntype Error struct {\n\tId               string      `json:\"id\"`                         // Id is unique for a specific error, e.g. mydomain.not.assigned\n\tMessage          string      `json:\"message\"`                    // Message is a string for the developer\n\tLocalizedMessage string      `json:\"localizedMessage,omitempty\"` // LocalizedMessage is something to display the user\n\tCausedBy         *Error      `json:\"causedBy,omitempty\"`         // CausedBy returns an optional root error\n\tType             string      `json:\"type,omitempty\"`             // Type is a developer notice for the internal inspection\n\tDetails          interface{} `json:\"details,omitempty\"`          // Details contains arbitrary payload\n}\n\n// ParseError tries to parse the response as json. In any case it returns an error.\nfunc ParseError(reader io.Reader) *Error {\n\tbuf, err := ioutil.ReadAll(reader)\n\tif err != nil {\n\t\treturn AsError(err)\n\t}\n\n\tres := &Error{}\n\terr = json.Unmarshal(buf, res)\n\tif err != nil {\n\t\treturn AsError(err)\n\t}\n\n\treturn res\n}\n\n// ID returns the unique error class id\nfunc (c *Error) ID() string {\n\treturn c.Id\n}\n\n// Error returns the message\nfunc (c *Error) Error() string {\n\treturn c.Message\n}\n\n// LocalizedError is like Error but translated or empty\nfunc (c *Error) LocalizedError() string {\n\treturn c.LocalizedMessage\n}\n\n// Class returns the technical type\nfunc (c *Error) Class() string {\n\treturn c.Type\n}\n\n// Payload returns the details\nfunc (c *Error) Payload() interface{} {\n\treturn c.Details\n}\n\n// Unwrap returns the cause or nil\nfunc (c *Error) Unwrap() error {\n\tif c.CausedBy == nil { // otherwise error iface will not be nil, because of the type info in interface\n\t\treturn nil\n\t}\n\treturn c.CausedBy\n}\n\nfunc AsError(err error) *Error {\n\tif e, ok := err.(*Error); ok {\n\t\treturn e\n\t}\n\n\te := &Error{}\n\te.Type = reflect.TypeOf(err).String()\n\te.Message = err.Error()\n\n\tif code, ok := err.(interface{ ID() string }); ok {\n\t\te.Id = code.ID()\n\t} else {\n\t\te.Id = e.Type\n\t}\n\n\tif details, ok := err.(interface{ Payload() interface{} }); ok {\n\t\te.Details = details\n\t}\n\n\tif localized, ok := err.(interface{ LocalizedError() string }); ok {\n\t\te.LocalizedMessage = localized.LocalizedError()\n\t}\n\n\tif class, ok := err.(interface{ Class() string }); ok {\n\t\te.Type = class.Class()\n\t}\n\n\tif wrapper, ok := err.(interface{ Unwrap() error }); ok {\n\t\tcause := wrapper.Unwrap()\n\t\tif cause != nil {\n\t\t\ttmp := AsError(cause)\n\t\t\te.CausedBy = tmp\n\t\t}\n\t}\n\n\treturn e\n}"
